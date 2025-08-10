bea824ed7aaf8ca27bd657b7bbb59cd860b1ec27
controlumseguridad <controlumseguridad@gmail.com>
2025-08-10 17:55:13 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Example from "./pages/Example";
+import Clients from "./pages/Clients";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Navbar />
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/example" element={<Example />} />
-        </Routes>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <div className="p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/example" element={<Example />} />
+            <Route path="/clients" element={<Clients />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
-import Clients from "./pages/Clients"; // ⬅️ Import nuevo
+
+export default App;
